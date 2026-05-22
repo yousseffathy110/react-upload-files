d14a91470d4dd31bd35a81212e42376084f4db57
yousseffathy110 <yf1102003@gmail.com>
2026-05-22 23:39:25 +0300

    Update file input accept types to support specific image formats

--- components/UploadInput.tsx
+++ components/UploadInput.tsx
@@ -1,7 +1,13 @@
 export function UploadInput() {
   return (
     <label htmlFor="file-input">
-      <input type="file" accept="image/*" id="file-input" hidden multiple />
+      <input
+        type="file"
+        accept=".png,.jpg,.jpeg,.svg,.gif"
+        id="file-input"
+        hidden
+        multiple
+      />
       <span className="text-sm md:text-base font-semibold text-[#6941c6] hover:underline cursor-pointer">
         Click to upload and attach files
       </span>
